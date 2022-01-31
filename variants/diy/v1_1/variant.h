a2792fbd95f551db117c4071de63874653cf9b92
Vladislav Osmanov <info@auxmic.com>
2022-01-31 16:53:39 +0300

    GPIO4 custom version

--- variants/diy/v1_1/variant.h
+++ variants/diy/v1_1/variant.h
@@ -1,35 +1,40 @@
 // For OLED LCD
+//#undef RESET_OLED
 #define I2C_SDA 21
 #define I2C_SCL 22
 
 // GPS
 #undef GPS_RX_PIN
 #define GPS_RX_PIN 15
+#undef GPS_TX_PIN
+#define GPS_TX_PIN 24 // 4 // 12 // not connected
 
-#define BUTTON_PIN 2 // The middle button GPIO on the T-Beam
+#define BUTTON_PIN 2 // 39 // The middle button GPIO on the T-Beam
 #define BUTTON_NEED_PULLUP
-#define EXT_NOTIFY_OUT 12 // Overridden default pin to use for Ext Notify Plugin (#975).
 
+#define EXT_NOTIFY_OUT 13 // 12 // Overridden default pin to use for Ext Notify Plugin (#975).
+
+#define LORA_CS 18    // CS/NSS for SX126X
 #define LORA_DIO0 26  // a No connect on the SX1262/SX1268 module
-#define LORA_RESET 27 // RST for SX1276, and for SX1262/SX1268
-#define LORA_DIO1 33  // IRQ for SX1262/SX1268
-#define LORA_DIO2 32  // BUSY for SX1262/SX1268
+#define LORA_RESET 27 // 23 // RST for SX1276, and for SX1262/SX1268
+#define LORA_DIO1 12  // 33  // IRQ for SX1262/SX1268
+#define LORA_DIO2 14  // 32  // BUSY for SX1262/SX1268
 #define LORA_DIO3     // Not connected on PCB, but internally on the TTGO SX1262/SX1268, if DIO3 is high the TXCO is enabled
 
 // In transmitting, set TXEN as high communication level，RXEN pin is low level;
 // In receiving, set RXEN as high communication level, TXEN is lowlevel;
 // Before powering off, set TXEN、RXEN as low level.
-#define LORA_RXEN 14  // Input - RF switch RX control, connecting external MCU IO, valid in high level
-#define LORA_TXEN 13  // Input - RF switch TX control, connecting external MCU IO or DIO2, valid in high level
+#define LORA_RXEN 17  // Input - RF switch RX control, connecting external MCU IO, valid in high level
+#define LORA_TXEN 4  // Input - RF switch TX control, connecting external MCU IO or DIO2, valid in high level
 
 #undef RF95_SCK
-#define RF95_SCK 18
+#define RF95_SCK 5
 #undef RF95_MISO
 #define RF95_MISO 19
 #undef RF95_MOSI
 #define RF95_MOSI 23
 #undef RF95_NSS
-#define RF95_NSS 5
+#define RF95_NSS LORA_CS
 
 // RX/TX for RFM95/SX127x
 #define RF95_RXEN LORA_RXEN
@@ -37,7 +42,7 @@
 // #define RF95_TCXO <GPIO#>
 
 // common pinouts for SX126X modules
-#define SX126X_CS 5
+#define SX126X_CS LORA_CS
 #define SX126X_DIO1 LORA_DIO1
 #define SX126X_BUSY LORA_DIO2
 #define SX126X_RESET LORA_RESET
